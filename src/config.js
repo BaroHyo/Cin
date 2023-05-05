91e351bdc745624e53d3310a33d8b1fe63736e67
etr <etr@MacBook-Pro-de-etr.local>
2023-05-05 08:25:34 -0400

    Home Ui

---
+++ src/config.js
@@ -0,0 +1,37 @@
+export const menu = [
+  {
+    id: 1,
+    nombre: 'PRODUCTOS',
+    component: 'Producto',
+  },
+  {
+    id: 2,
+    nombre: 'CLIENTES',
+    component: 'Cliente',
+  },
+  {
+    id: 3,
+    nombre: 'PEDIDOS',
+    component: 'Pedido',
+  },
+  {
+    id: 4,
+    nombre: 'REGISTRADOS',
+    component: '',
+  },
+  {
+    id: 5,
+    nombre: 'VISITA',
+    component: 'VisitasScreen',
+  },
+  {
+    id: 6,
+    nombre: 'MAPA GPS.',
+    component: 'Mapa',
+  },
+  {
+    id: 7,
+    nombre: 'DISTRIBUCIÓN',
+    component: 'TabDestribucion',
+  },
+];
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { Center } from 'native-base';
+import { Card } from '../../components';
+
+const menu = [1, 2, 3, 4, 5, 6, 7];
 
 export function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <Center flex={1}>
+      <FlatList
+        data={menu}
+        keyExtractor={(p, i) => i}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        renderItem={({ item }) => <Card />}
+        onEndReachedThreshold={0.4}
+      />
+    </Center>
   );
 }
